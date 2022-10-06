5e3aaf8ab340bb92abd7fcaa2376a133143adafb
Antoine Goutenoir <antoine@goutenoir.com>
2022-10-06 03:10:58 +0200

    Use load instead of preload
    fix #13

--- addons/goutte.camera.trackball/trackball_camera_plugin.gd
+++ addons/goutte.camera.trackball/trackball_camera_plugin.gd
@@ -4,8 +4,8 @@ extends EditorPlugin
 func _enter_tree():
 	add_custom_type(
 		"TrackballCamera", "Camera",
-		preload("res://addons/goutte.camera.trackball/trackball_camera.gd"),
-		preload("res://addons/goutte.camera.trackball/icon_trackball_camera.png")
+		load("res://addons/goutte.camera.trackball/trackball_camera.gd"),
+		load("res://addons/goutte.camera.trackball/icon_trackball_camera.png")
 	)
 
 func _exit_tree():
